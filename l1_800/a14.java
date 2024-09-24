8bd987745b666d9786b014520b238a50ce8b6c95
Laba Kumar Deka <labakumardeka42@gmail.com>
2024-09-24 10:56:14 -0400

    added solutions till 17

--- l1_800/a14.java
+++ l1_800/a14.java
@@ -13,35 +13,6 @@ public class a14 {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
-            
-            if(a>b){
-
-
-                if(a-b == 1){
-                    if(c%2 != 0 ){
-                        System.out.println("Second");
-                    }else{
-                        System.out.println("First");
-                    }
-                }else{
-                    System.out.println("First");
-                }
-
-            }else if (b>a){
-                System.out.println("Second");
-            }else{
-                // if same
-
-
-                    if(c%2 == 0 ){
-                        System.out.println("Second");
-                    }else{
-                        System.out.println("First");
-                    }
-
-
-            }
-
 
 
         }
